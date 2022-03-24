41af754616cab64e988bfefc3790508a484cde7d
landy8697 <landy8697@gmail.com>
2022-03-24 18:44:59 -0500

    a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cheerio = require("cheerio");
 const axios = require("axios").default;
+const wiki = require('wikipedia');
 const fetchHtml = async url => {
 	try {
 		const { data } = await axios.get(url);
@@ -11,12 +12,58 @@ const fetchHtml = async url => {
 
 const scrapWiki = async () => {
 	const wikiUrl =
-		"https://en.wikipedia.org/wiki/Batman";
+		"https://en.wikipedia.org/wiki/Cognition";
 
 	var html = await fetchHtml(wikiUrl);
+	html = html.substring(html.indexOf('h1'))
+	const $ = cheerio.load(html);
+	
+	//while(html.includes('<table')){
+	//	html = html.substring(0, html.indexOf('<table')+6)+html.substring('</table>')
+	//}
+	/*
+	$('table').each((i, link) => {
+
+		//console.log($(this).html)
+		$(this).html('');
+	});
+	html = $.html()
+	*/
+	//console.log(html);
+	//console.log(html==='hi')
 	html = html.substring(html.indexOf('<p>'))
 	html = html.substring(0, html.indexOf('</p>'));
-	console.log(html);
+	console.log(html)
+	isLink = false;
+	openParen = false;
+	for(var s of html.split(' ')){
+		
+		if(s.includes('('))openParen = true;
+		if(s.includes(')'))openParen = false;
+		if(openParen)continue;
+		if(!s.includes('<a') && !isLink)continue;
+		if(s == '<a'){
+			isLink = true;
+			continue;
+		}
+		if(s.includes('<a'))isLink = true;
+		if(s.includes('</a>'))isLink = false;
+		if(s.includes('reference'))continue;
+		if(s.includes('cite_note'))continue;
+
+		
+		console.log(s);
+		if(s.includes('href'))break;
+		//console.log(s);
+	}
+	/*
+	while(html.includes('<a')){
+		html = html.substring(html.indexOf('<a')+2)
+		href = 
+		console.log(html)
+	}
+	*/
+
 	/*
 	const wikiUrl =
 		"https://en.wikipedia.org/wiki/Batman";
